9fb3bc898d622f8e77e2387ddc6165334b7b2f15
jkol36 <jonathankolman@gmail.com>
2015-05-05 23:29:57 -0400

    ready

--- groupster/settings.py
+++ groupster/settings.py
@@ -64,7 +64,6 @@ DATABASES = {
         'NAME': 'groupster_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'groupster',
-        'PASSWORD': 'J0nnyb0y123',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
