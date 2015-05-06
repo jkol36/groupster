a1d1e8822ed1f4ea89c54478f83e0eb9e74d2ca8
jkol36 <jonathankolman@gmail.com>
2015-05-05 22:54:00 -0400

    ready

--- groupster/settings.py
+++ groupster/settings.py
@@ -60,10 +60,16 @@ WSGI_APPLICATION = 'groupster.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'groupster_db',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'groupster_django',
+        'PASSWORD': 'StoreTheGroupster',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
+
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
